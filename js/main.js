64359e87081475f5dcd85f7e78e3b558356f7682
RangiBecerra <rangibecerra@gmail.com>
2017-04-27 13:54:43 -0300

    lista con objeto

--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
 var res = [];
     // me elige los 10 primeros
     for (var i = 0; i < 10; i++) {
-        // se agrega valor al mi nuevo array
+        // se agrega valor a mi nuevo array
         res.push(arr[i]);
     }
-//document.write(res); esta leyendo 10
+console.log(res); // esta leyendo 10
+// Me arroja el atributo title de cada objeto
 res.forEach(function(el){
 	var contarea = document.getElementsByClassName("list");
 	contarea[0].innerHTML += "<li>" + el.title + "<br>" + "</li>";
 	
   });
-// document.getElementById("tareas").innerHTML = "<li>" + el.title + "<br>" + "</li>";
+// Ejercicio en Clases
 function agregarTareas(){
 	var input = document.getElementById("tarea");
-
 	var contarea = document.getElementsByClassName("list");
-
 	contarea[0].innerHTML += "<li>" + input.value + "</li>";
-
+	//res.push(input.value);
+	res.push({
+		'Id': res.length,
+        'userId': 1,
+        'title': input.value,
+        'completed': false
+    });
+	console.log(res);
 	input.value = "";
 }

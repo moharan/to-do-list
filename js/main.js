d6beaef4e18fc4d5dc9ed71c3dc67c7575a9fbdc
RangiBecerra <rangibecerra@gmail.com>
2017-04-27 12:28:35 -0300

    lista trabajando

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-arr.forEach(function(el){
-	//document.write("<img src='" + el.listing.thumbnail_url + "'" + "><br>");
-	document.write("<div>" + "<b>Datos:</b> " + el.name + "<br>" + "<b>Calle :</b> " + el.address.street + " <br><b>Ciudad:</b>  " + el.address.city + "</div>");
+var res = [];
+    // me elige los 10 primeros
+    for (var i = 0; i < 10; i++) {
+        // se agrega valor al mi nuevo array
+        res.push(arr[i]);
+    }
+//document.write(res); esta leyendo 10
+res.forEach(function(el){
+	var contarea = document.getElementsByClassName("list");
+	contarea[0].innerHTML += "<li>" + el.title + "<br>" + "</li>";
+	
   });
+// document.getElementById("tareas").innerHTML = "<li>" + el.title + "<br>" + "</li>";
+function agregarTareas(){
+	var input = document.getElementById("tarea");
+
+	var contarea = document.getElementsByClassName("list");
+
+	contarea[0].innerHTML += "<li>" + input.value + "</li>";
+
+	input.value = "";
+}
